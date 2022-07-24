13ee3e473cde8891354963f7e06baf53761fbf35
metkaergul <metkaergul@gmail.com>
2022-07-24 20:51:22 +0300

    updated

--- src/constructors/Employee.java
+++ src/constructors/Employee.java
@@ -14,8 +14,8 @@ public class Employee {
         salary = 10000;  // when I create an Employee object with default constructor, their salary will be 10000 as default.
   }
 
-    public Employee(String n, String jobTitle, int ID, double salary) {
-        name = n;
+    public Employee(String name, String jobTitle, int ID, double salary) {
+        this.name =name ;
         this.jobTitle = jobTitle;
         this.ID = ID;
         this.salary = salary;
